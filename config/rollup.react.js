31aee8a4874bb9a21263787d975279aba7712e83
Nico Rehwaldt <git_nikku@nixis.de>
2019-08-19 20:05:45 +0200

    fix(project): correct react setup

--- config/rollup.react.js
+++ config/rollup.react.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
 
 import babel from 'rollup-plugin-babel';
 
@@ -12,6 +13,9 @@ export default {
     file: 'build/2019-react.js'
   },
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     babel({
       exclude: 'node_modules/**',
       presets: [
